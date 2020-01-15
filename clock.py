42db9d6c87f03197ac0ac184aaf1dcad100df238
esun <esun@flexion.us>
2020-01-15 11:18:15 -0500

    add f1 team channel

--- clock.py
+++ clock.py
@@ -9,12 +9,20 @@ sched = BlockingScheduler(timezone="America/New_York")
 # def timed_job():
 #     print('This job is run every three minutes.')
 
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
+def scheduled_f1_job():
+  lucky_winner = google_util.get_daily_scrum_lucky_winner()
+  today = date.today().strftime("%b %-d, %Y")
+  slack_util.slack_message(f'_{today}_ - *{lucky_winner}*, you are today\'s lucky winner!', '#test-zapier', 'Daily Scrum Bot', ':alarm_clock:')
+  print('This job is run every weekday at 10am.')
+
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
 def scheduled_job():
   lucky_winner = google_util.get_daily_scrum_lucky_winner()
   today = date.today().strftime("%b %-d, %Y")
-  slack_util.slack_message(f'_{today}_ - *{lucky_winner}*, you are today\'s lucky winner!', '#test-zapier', 'Daily Scrum Bot', ':alarm_clock:')
+  slack_util.slack_message(f'_{today}_ - *{lucky_winner}*, you are today\'s lucky winner!', '#drc-f1-app-team', 'Daily Scrum Bot', ':alarm_clock:')
   print('This job is run every weekday at 10am.')
+
 
 sched.start()
